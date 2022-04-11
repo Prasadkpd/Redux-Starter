a4516d27eba5c72b651c61da9d1045cbeb3b8744
Prasad Lakshan <77828396+Prasadkpd@users.noreply.github.com>
2022-04-11 19:13:29 +0530

    redux thunk middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,6 +1,8 @@
 const redux = require('redux');
-const createStroe = redux.createStore;
-
+const applyMiddleware = redux.applyMiddleware;
+const createStore = redux.createStore;
+const thunkMiddleware = require('redux-thunk').default;
+const axios = require('axios');
 
 const initialState = {
     loading: false,
@@ -58,4 +60,22 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStroe(reducer);
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequest());
+        axios.get('https://jsonplaceholder.typicode.com/user')
+            .then(response => {
+                // response.data is the array of users
+                const users = response.data.map(user => user.id);
+                dispatch(fetchUsersSuccess(users));
+            })
+            .catch(error => {
+                //error.message is the error description
+                dispatch(fetchUsersFailure(error.message));
+            })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => {console.log(store.getState())});
+store.dispatch(fetchUsers());
